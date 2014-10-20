f051164a20b1a10c719d0d6cd9bc99be772580b0
David Wilkinson <david.wilkinson1984@gmail.com>
2014-10-20 18:31:56 +0100

    Program scaffolded

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 ScheduleApp::Application.routes.draw do
+  resources :programs
+
+
   resources :classrooms
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141020171037) do
+ActiveRecord::Schema.define(:version => 20141020173103) do
 
   create_table "classrooms", :force => true do |t|
     t.string   "name"
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(:version => 20141020171037) do
     t.text     "full_address"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+  end
+
+  create_table "programs", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
