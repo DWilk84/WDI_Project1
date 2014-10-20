fdb2f3a8f5cc680092cca79c9261e23b7a5525f9
David Wilkinson <david.wilkinson1984@gmail.com>
2014-10-20 18:07:17 +0100

    seeded locations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Location.destroy_all
+
+Location.create!(name: "GA London @ White Bear Yard", city: "London", country: "England", full_address: "GA London @ White Bear Yard, 9 Back Hill, 4th Floor, London, EC1R 5EN")
+
+Location.create!(name: "GA New York City (West)", city: "New York", country: "USA", full_address: "GA New York City (West), 10 East 21st Street, 4th Floor, New York, 10010")
