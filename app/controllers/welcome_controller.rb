5996d8d3123d24d0a816357e5748e36307878ed7
David Wilkinson <david.wilkinson1984@gmail.com>
2014-10-24 09:13:34 +0100

    added room detail to the calendar event in bookings helper

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,9 @@
 class WelcomeController < ApplicationController
   
   def index
-    if current_user
-      redirect_to home_url
-    end
+    # if current_user
+    #   redirect_to home_url
+    # end
   end
 
 end
